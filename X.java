bee096d2ac7be861be12d3b7e55c49b4f4893030
irineumouracaldeira <121524120+irineumouracaldeira@users.noreply.github.com>
2024-08-14 15:23:20 -0300

    Update X.java Obs. Erro ! Falta ponto e virgula

--- X.java
+++ X.java
@@ -1,15 +1,5 @@
 class X {
-	  static void main (String[] args) {
-	/* comentário daqui,
-	ate aqui */
-	
-	// uma linha de comentário
-	int i = 5; // i recebe uma cópia do valor 5
-	int j = i; // j recebe uma cópia do valor de i
-	i = i + 1; //i vira 6, j continua 5
-	
-	System.out.println(i);
-	System.out.println(j);
-
+	public static void main (String[] args) {
+		System.out.println("Falta ponto e vírgula")
 	}
 }
