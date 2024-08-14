837a1dc4111011e8e8cbd0329b671d5df898fdce
irineumouracaldeira <121524120+irineumouracaldeira@users.noreply.github.com>
2024-08-14 14:59:21 -0300

    Add files via upload

---
+++ X.java
@@ -0,0 +1,15 @@
+class X {
+	  static void main (String[] args) {
+	/* comentário daqui,
+	ate aqui */
+	
+	// uma linha de comentário
+	int i = 5; // i recebe uma cópia do valor 5
+	int j = i; // j recebe uma cópia do valor de i
+	i = i + 1; //i vira 6, j continua 5
+	
+	System.out.println(i);
+	System.out.println(j);
+
+	}
+}
